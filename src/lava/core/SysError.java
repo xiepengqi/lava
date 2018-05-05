958c59c372b5d2daf5b366613382896c14284b45
xiepengqi <xie.pq@outlook.com>
2018-05-05 22:52:33 +0800

    exception

--- src/lava/core/SysError.java
+++ src/lava/core/SysError.java
@@ -1,10 +1,17 @@
 package lava.core;
 
+import lava.constant.Constants;
+
 /**
  * Created by xie,pengqi on 2017/12/21.
  */
 public class SysError extends RuntimeException{
-    public SysError(Form form, String msg){
-        super(form.getWhere() + ":" + form.see() + ":" + msg);
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SysError(Form form, String msg){
+        super(form.getWhere() + ":" + form.see() + Constants.newLine + "	" + msg);
     }
 }
